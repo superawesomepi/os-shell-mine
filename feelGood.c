44bea79b1a8cfb88653838ff6ca88fb67c8d5ac4
Jordan <112716145+superawesomepi@users.noreply.github.com>
2024-02-16 14:20:19 -0800

    Initial commit

---
+++ feelGood.c
@@ -0,0 +1,14 @@
+/**
+ * Do not modify
+ */
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+  while (1)
+  {
+    printf("Students think you're inspiring!\n");
+    sleep(4);
+  }
+  return 0;
+}
